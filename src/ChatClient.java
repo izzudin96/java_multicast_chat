d5df4f19cfc7f83604cf5873b8108f91f8f8b014
Izzudin Anuar <izzudinanuar96@gmail.com>
2018-10-01 13:27:05 +1300

    fix spacing due to editing in lab using gedit, and successfully verified

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -12,13 +12,13 @@ class ChatClient {
      */
     public static void main(String[] args) {
         System.out.println("System client on " + multicast_address + ":" + port);
-            try {
-                System.setProperty("java.net.preferIPV4Stack", "true");
-                ChatClient chatClient = new ChatClient();
-                chatClient.startClient();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+
+        try {
+            ChatClient chatClient = new ChatClient();
+            chatClient.startClient();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -30,17 +30,17 @@ class ChatClient {
         InetAddress group = InetAddress.getByName(multicast_address);
         MulticastSocket multicastSocket = new MulticastSocket(port);
         multicastSocket.joinGroup(group);
-        ReadThread readThread = new ReadThread(group, port, multicastSocket);
+        ReadThread readThread = new ReadThread(group, port);
         readThread.start();
-	while(true) {
-		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        	String inputMessage = bufferedReader.readLine();
-	        byte[] message = inputMessage.getBytes();
 
-        	DatagramPacket packet = new DatagramPacket(message, message.length, group, port);
-        	multicastSocket.send(packet);
-	}
+	    while(true) {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            String inputMessage = bufferedReader.readLine();
+            byte[] message = inputMessage.getBytes();
 
+            DatagramPacket packet = new DatagramPacket(message, message.length, group, port);
+            multicastSocket.send(packet);
+	    }
     }
 }
 
@@ -50,27 +50,25 @@ class ChatClient {
 class ReadThread extends Thread {
     InetAddress group;
     int port;
-	MulticastSocket multicastSocket;
 
-    ReadThread(InetAddress group, int port, MulticastSocket multicastSocket) {
+    ReadThread(InetAddress group, int port) {
         this.group = group;
         this.port = port;
-	this.multicastSocket = multicastSocket;
     }
 
     public void run() {
         try {
-	while(true) {
-            MulticastSocket read = new MulticastSocket(port);
-            read.joinGroup(group);
+            while(true) {
+                MulticastSocket read = new MulticastSocket(port);
+                read.joinGroup(group);
 
-            byte[] buffer = new byte[100];
-            DatagramPacket receivedPacket = new DatagramPacket(buffer, buffer.length, group, port);
-            read.receive(receivedPacket);
+                byte[] buffer = new byte[100];
+                DatagramPacket receivedPacket = new DatagramPacket(buffer, buffer.length, group, port);
+                read.receive(receivedPacket);
 
-            String receivedMessage = new String(receivedPacket.getData());
-            System.out.println(receivedPacket.getAddress().toString() + ": " + receivedMessage);
-	}
+                String receivedMessage = new String(receivedPacket.getData());
+                System.out.println(receivedPacket.getAddress().toString() + ": " + receivedMessage);
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
